9d26dd2779fafc719a576043a0f105539604d7f6
Mohit Verma <mohitverma4109@gmail.com>
2025-01-16 16:32:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-# Monolithic-Project
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance.one" {
+count = 4
+ami = "ami-0973769d2c4406635"
+instance_type = "t2.medium"
+key_name = "mumkp"
+vpc_security_groups_ids = "sg-06537e734092f450d"
+
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+variable "instance_names" {
+default = ["Jenkins", "Nexus", "tomcat", "Monitoring"]
+}
